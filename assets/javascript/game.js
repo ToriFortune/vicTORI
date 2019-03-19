6f6c5b9a4f86927e60ea734ad23b00fc3b6e0205
Tori Fortune <toriolley@gmail.com>
2019-03-18 21:04:40 -0500

    <div id="musicControls" class="text-center">           <button class="btn btn-warning btn-xs theme-button">             <span class="fa fa-music"></span> Play Theme!</button>           <button class="btn btn-dark btn-xs pause-button">             <span class="fa fa-pause"></span> Pause Song</button>         </div>

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+// As a user, I should see instructions of how to play.
+// As a user, I should be presented with the target number for winning the game.
+// As a user, I should be able to click on an item and see my score increase by a determined but unknown amount.
+// As a user, I should see that I won the game when my score matches the random number chosen at the beginning.
+// As a user, I should see that I lost the game when my score goes over the random number chosen at the beginning.
+// As a user, I should be able to play a new game when I win or lose the current game.
+// As a user, I should be presented with a new target number for each game.
+// As a user, I should know how many times I won and lost the game.
